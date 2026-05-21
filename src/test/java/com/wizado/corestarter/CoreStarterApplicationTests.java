711cf327d966115f3deebc373b5410a78f2532f8
minhtq <minhtranquang2197@gmail.com>
2026-05-22 00:26:49 +0700

    first commit

---
+++ src/test/java/com/wizado/corestarter/CoreStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizado.corestarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
